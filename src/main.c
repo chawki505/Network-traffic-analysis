0e78c9bc5a0d691be484351ed07c8651625223d9
Chawki CHOUIB <chouibc@gmail.com>
2020-12-31 20:46:43 +0100

    refactoring code

--- src/main.c
+++ src/main.c
@@ -1,30 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include <pcap/pcap.h>
-
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <net/ethernet.h>
-#include <netinet/tcp.h>
-#include <netinet/udp.h>
-#include <arpa/inet.h>
 
 #include "utils.h"
-#include "dns.h"
-#include "http.h"
 
-void packetHandler(struct pcap_pkthdr *header, const u_char *packet);
-
-
+//main
 int main(int argc, char *argv[]) {
     const char *input_file = NULL;
     pcap_t *packets = NULL;
     const u_char *packet = NULL;
     struct pcap_pkthdr *header = NULL;
     char errbuf[PCAP_ERRBUF_SIZE];
-
+    system("clear");
     printf("- Checking arg count ...\n");
     //test input arg
     if (argc != 2) {
@@ -48,8 +32,10 @@ int main(int argc, char *argv[]) {
 
     printf("- Opened %s attempting to read packet lengths ...\n", input_file);
 
-    int compteur = 0;
+    int count = 0;
     header = malloc(sizeof(struct pcap_pkthdr));
+    struct pcap_pkthdr *save_header = header; //use in free();
+
 
     // Check if the memory has been successfully allocated by malloc or not
     if (header == NULL) {
@@ -59,21 +45,25 @@ int main(int argc, char *argv[]) {
     //init struct with 0
     memset(header, 0, sizeof(struct pcap_pkthdr));
 
+
     //print all packets
     while (pcap_next_ex(packets, &header, &packet) != -2) {
-        printf("\n\t%d - Packet of length [%d bytes] [%d bits]\n", compteur, header->len, header->len * 8);
+        printf("\n\t%d - Packet of length [%d bytes] [%d bits]\n", count + 1, header->len, header->len * 8);
         printf("\n\t");
         for (int i = 0; i < header->len; i++) {
             if (i % 16 == 0 && i != 0) printf("\n\t");
             printf("%02X ", packet[i]);
         }
-        compteur++;
+        count++;
         printf("\n\n");
         packetHandler(header, packet);
+        printf("\nPress enter to continue ... \n");
+        getchar();
+        system("clear");
     }
     printf("- Finish !\n");
 
-    //free(header);
+    free(save_header);
 
     //close packet file
     pcap_close(packets);
@@ -82,117 +72,4 @@ int main(int argc, char *argv[]) {
     printf("\tClosing %s OK!\n", input_file);
 
     return EXIT_SUCCESS;
-}
-
-void packetHandler(struct pcap_pkthdr *header, const u_char *packet) {
-
-    const struct ether_header *ethernetHeader;
-    const struct ip *ipHeader;
-
-    const struct tcphdr *tcpHeader;
-    const struct udphdr *udpHeader;
-
-    char sourceIP[INET_ADDRSTRLEN];
-    char destIP[INET_ADDRSTRLEN];
-
-    u_int sourcePort, destPort;
-    u_char *data;
-
-    size_t dataLength = 0;
-
-    //ethernet fragment
-    ethernetHeader = (struct ether_header *) packet;
-
-    if (ntohs(ethernetHeader->ether_type) == ETHERTYPE_IP) {
-
-        //ip fragment
-        ipHeader = (struct ip *) (packet + sizeof(struct ether_header));
-
-        inet_ntop(AF_INET, &(ipHeader->ip_src), sourceIP, INET_ADDRSTRLEN);
-        inet_ntop(AF_INET, &(ipHeader->ip_dst), destIP, INET_ADDRSTRLEN);
-
-
-        //TCP fragment
-        if (ipHeader->ip_p == IPPROTO_TCP) {
-            tcpHeader = (struct tcphdr *) (packet + sizeof(struct ether_header) + sizeof(struct ip));
-            sourcePort = ntohs(tcpHeader->th_sport);
-            destPort = ntohs(tcpHeader->th_dport);
-
-            data = (u_char *) (packet + sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct tcphdr));
-            dataLength = header->len - (sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct tcphdr));
-
-            //http port = 80
-            if (sourcePort == 80 || destPort == 80) {
-                printf("======================= HTTP PACKET =======================\n\n");
-                //print http protocol
-                struct Http_Request *req = http_parse_request((char *) data, dataLength);
-                if (req) {
-                    printf("Method: %s\n", req->method);
-                    printf("Request-URI: %s\n", req->url);
-                    printf("HTTP-Version: %s\n", req->version);
-                    printf("Headers:\n");
-                    struct Http_Header *h;
-                    for (h = req->headers; h; h = h->next) {
-                        printf("\t%s: %s\n", h->name, h->value);
-                    }
-                    printf("message-body: %s\n", req->body);
-                } else {
-                    struct Http_Response *resp = http_parse_response((char *) data, dataLength);
-                    if (resp) {
-                        printf("HTTP-Version: %s\n", resp->version);
-                        printf("Status code: %s\n", resp->status_code);
-                        printf("Status text: %s\n", resp->status_text);
-                        printf("Headers:\n");
-                        struct Http_Header *h;
-                        for (h = resp->headers; h; h = h->next) {
-                            printf("\t%s: %s\n", h->name, h->value);
-                        }
-                        printf("message-body: %s\n", resp->body);
-                    }
-                    http_free_response(resp);
-                }
-                http_free_request(req);
-
-                printf("==========================================================\n");
-            }
-
-            if (tcpHeader->th_flags & TH_SYN) {
-                //print syn tcp
-            }
-
-            if (tcpHeader->th_flags & TH_ACK) {
-                //print ack tcp
-            }
-
-            if (tcpHeader->th_flags & TH_ACCEPT) {
-                //print ack accept
-            }
-
-            //UDP fragment
-        } else if (ipHeader->ip_p == IPPROTO_UDP) {
-            udpHeader = (struct udphdr *) (packet + sizeof(struct ether_header) + sizeof(struct ip));
-            sourcePort = ntohs(udpHeader->uh_sport);
-            destPort = ntohs(udpHeader->uh_dport);
-
-            data = (u_char *) (packet + sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct udphdr));
-            dataLength = header->len - (sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct udphdr));
-
-            if (sourcePort == 53 || destPort == 53) {
-
-                printf("======================= DNS PACKET =======================\n\n");
-                dns_print_header(data);
-                if (dns_get_type(data) == 1) {
-                    struct dns_response *a = dns_get_answer(data, dataLength);
-                    dns_print_answer(a);
-                    free(a);
-                } else {
-                    struct dns_query *q = dns_get_query(data, dataLength);
-                    dns_print_query(q);
-                    free(q);
-                }
-                printf("==========================================================\n");
-
-            }
-        }
-    }
 }
